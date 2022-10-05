52400bf541bcef43831e366c3c3965a2111d4381
Donadominic <43172188+Donadominic@users.noreply.github.com>
2022-10-05 10:20:51 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,18 @@
 package com.driver;
-
+class Product{
+        public int product(int x,int y) {
+            return x*y;
+        }
+        public int product(int x,int y,int z) {
+            return x*y*z;
+        }
+    }
 public class Main {
-
+    
+    public static void main(String[]args) {
+        Product p=new Product();
+        p.product(5,6);
+        p.product(5,6,7);
+    }
     
   }
